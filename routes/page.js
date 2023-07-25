14c20342895a3b9d673e3db7daad10ed5a20bb57
chanhyun.lee <ech97@naver.com>
2023-07-25 21:10:19 +0900

    add sequelize model

--- routes/page.js
+++ routes/page.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('');
+router.use((req, res, next) => {
+    // 넌적스에서 사용할 변수 설정
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followingIdList = [];
+    next();
+});
+
+router.get('/profile', renderProfile);
+router.get('/join', renderJoin);
+router.get('/', renderMain);
+
+module.exports = router;
